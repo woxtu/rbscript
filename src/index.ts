292dc03f54b3e5b9e8fd4cd6d67e795f84f91b1f
woxtu <woxtup@gmail.com>
2022-05-29 00:48:34 +0900

    Add event listeners

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
 import type { RubyVM } from "ruby-head-wasm-wasi";
+import { events } from "./event";
 import { createVM } from "./vm";
 
 async function launch(vm: RubyVM): Promise<RubyVM> {
+  // Add event listeners
+  function addEventListener(target: HTMLElement): void {
+    for (const event of events) {
+      for (const element of [target, ...target.querySelectorAll<HTMLElement>(`*[data-rb-on${event}]`)]) {
+        const method = element.dataset[`rbOn${event}`];
+        if (method) {
+          element.addEventListener(event, () => {
+            vm.eval(`send(:${method})`);
+          });
+        }
+      }
+    }
+  }
+
+  addEventListener(document.body);
+
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (node instanceof HTMLElement) {
+          addEventListener(node);
+        }
+      }
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+
+  // Execute Ruby codes
   for (const element of document.querySelectorAll<HTMLScriptElement>("script[type*='text/ruby']")) {
     if (element.src) {
       const response = await fetch(element.src);
