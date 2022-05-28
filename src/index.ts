856d74fd33f328af79c5ffd3416b96ec566739c7
woxtu <woxtup@gmail.com>
2022-05-29 00:21:03 +0900

    Execute Ruby codes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
+import type { RubyVM } from "ruby-head-wasm-wasi";
 import { createVM } from "./vm";
 
-createVM().then((vm) => {
-  vm.eval("puts 'Hello, world!'");
-});
+async function launch(vm: RubyVM): Promise<RubyVM> {
+  for (const element of document.querySelectorAll<HTMLScriptElement>("script[type*='text/ruby']")) {
+    if (element.src) {
+      const response = await fetch(element.src);
+      const content = await response.text();
+      vm.eval(content);
+    } else if (element.textContent) {
+      vm.eval(element.textContent);
+    }
+  }
+
+  return vm;
+}
+
+export const loadedVM: Promise<RubyVM> = createVM().then(launch);
