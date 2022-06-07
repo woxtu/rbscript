f1a91ceeafd7b3ebafbc87cc8399f2d62fbed82d
woxtu <woxtup@gmail.com>
2022-06-07 20:55:29 +0900

    Update Ruby VM

--- src/vm.ts
+++ src/vm.ts
@@ -1,11 +1,10 @@
 import type { RubyVM } from "ruby-head-wasm-wasi";
 // @ts-ignore
-import { DefaultRubyVM } from "ruby-head-wasm-wasi/dist/browser.umd.js";
+import { DefaultRubyVM } from "ruby-head-wasm-wasi/dist/browser.esm.js";
 
 export async function createVM(): Promise<RubyVM> {
   const response = await fetch(
-    // The latest version available at this time
-    "https://cdn.jsdelivr.net/npm/ruby-head-wasm-wasi@0.3.0-2022-05-10-a/dist/ruby+stdlib.wasm"
+    "https://cdn.jsdelivr.net/npm/ruby-head-wasm-wasi@0.3.0-2022-06-04-a/dist/ruby+stdlib.wasm"
   );
   const buffer = await response.arrayBuffer();
   const module = await WebAssembly.compile(buffer);
